3e058678014a7fb85853a101db358e91236955af
AReinemann <reineman18@up.edu>
2015-09-03 17:14:08 -0700

    Initial commit

--- app/src/main/java/edu/up/cs371/primetester/PrimeTester.java
+++ app/src/main/java/edu/up/cs371/primetester/PrimeTester.java
@@ -15,6 +15,28 @@ public class PrimeTester {
      */
     public static boolean isPrime(long n) {
         // for now, return a random result
-        return Math.random() > 0.5;
+        if(n < 2)
+        {
+            return false;
+        }
+        else if(n == 2)
+        {
+            return true;
+        }
+        else if ((n%2) == 0)
+        {
+            return false;
+        }
+        else
+        {
+            for(int i=3;i<n; i+=2)
+            {
+                if(n%i == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
     }
 }
